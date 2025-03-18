9e355d83cd98af67e2e22d0b3918ad1696f8eace
user <girithar@nearle.in>
2025-03-18 16:46:19 +0530

    Commit : 18-03-2025 1.Desktop Site With Screen Issue Check.

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -65,14 +65,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "b3b9257e9187e71878345b789e9001ee
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "286ca3a7b45ad753fe656d3b1dba0676",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "29a2a8354588fddec8fa91aaffdf92f6",
+"flutter_bootstrap.js": "04905d9fe9587b4715f7e9c636978024",
 "icons/Icon-192.png": "a712d8fba46108dc51912bb91233cad1",
 "icons/Icon-512.png": "696ad1825508b34e6a79d631b7704842",
 "icons/Icon-maskable-192.png": "a712d8fba46108dc51912bb91233cad1",
 "icons/Icon-maskable-512.png": "696ad1825508b34e6a79d631b7704842",
-"index.html": "7e0283e1cc9079698230e41954965b72",
-"/": "7e0283e1cc9079698230e41954965b72",
-"main.dart.js": "eab925a343f240e2477ead5214820305",
+"index.html": "aa02cd0c59c1df975a81cdaea46cab63",
+"/": "aa02cd0c59c1df975a81cdaea46cab63",
+"main.dart.js": "fdea43fca660681125324a78d617654d",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"};
 // The application shell files that are downloaded before a service worker can
