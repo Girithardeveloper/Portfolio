d15f861afcea5940fb7727005b2ee0c2e4549dbb
user <girithar@nearle.in>
2025-03-20 19:31:01 +0530

    Commit : 20-03-2025 1.Desktop Site With Screen Issue Check.

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "519919514"
+    serviceWorkerVersion: "1644980026"
   }
 });
--- lib/globalWidgets/responsiveSizeWidget.dart
+++ lib/globalWidgets/responsiveSizeWidget.dart
@@ -1,46 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'dart:math' as math;
 
-// class ResponsiveSize {
-//   ///Screen Breakpoints
-//   static const double largeDesktopWidth = 1920.0;
-//   static const double desktopWidth = 1024.0;
-//   static const double tabletWidth = 768.0;
-//   static const double mobileWidth = 768.0;
-//
-//   ///Screen factors for each device type
-//   static const double largeDesktopScale = 1.1;
-//   static const double desktopScale = 1.15;
-//   // static const double desktopScale = 1.15;
-//   static const double tabletScale = 1.05;
-//   static const double mobileScale = 0.95;
-//
-//   static double getSize(BuildContext context, double baseSize) {
-//     double screenWidth = MediaQuery.of(context).size.width;
-//     double pixelRadio = MediaQuery.of(context).devicePixelRatio;
-//
-//     ///Calculate Scale factor based on screen width
-//     double scaleFactor;
-//     if (screenWidth > largeDesktopWidth) {
-//       scaleFactor = (screenWidth / largeDesktopWidth) * largeDesktopScale;
-//     } else if (screenWidth > desktopWidth) {
-//       scaleFactor = (screenWidth / desktopWidth) * desktopScale;
-//     } else if (screenWidth > tabletWidth) {
-//       scaleFactor = (screenWidth / tabletWidth) * tabletScale;
-//     } else {
-//       scaleFactor = (screenWidth / mobileWidth) * mobileScale;
-//     }
-//
-//     ///Apply constraints
-//     scaleFactor = scaleFactor.clamp(0.75, 1.5);
-//     // scaleFactor = scaleFactor.clamp(0.85, 1.8);
-//
-//
-//     ///Calculate final size with pixel ratio compensation
-//     return math.max((baseSize * scaleFactor) / (pixelRadio > 2 ? pixelRadio * 0.7 : 1), 8.0);
-//   }
-// }
-
 class ResponsiveSize {
   /// Screen Breakpoints
   static const double largeDesktopWidth = 1920.0;
